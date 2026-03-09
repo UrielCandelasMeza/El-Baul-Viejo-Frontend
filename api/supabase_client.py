17fd5ec6a7ccdfc9c3d9ed24182ee81d699638f6
UrielCandelasMeza <candelas.meza.uriel@gmail.com>
2026-03-09 01:48:19 -0600

    Web añadido

--- api/supabase_client.py
+++
@@ -1,8 +0,0 @@
-"""Supabase Client"""
-from supabase import create_client, Client
-from config import Config
-
-url = Config.SUPABASE_URL
-key = Config.SUPABASE_KEY
-
-supabase: Client = create_client(url, key)
