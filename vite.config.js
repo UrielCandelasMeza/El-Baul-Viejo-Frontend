3413ef56eb30ede7120f95a6974ddc142765120c
UrielCandelasMeza <candelas.meza.uriel@gmail.com>
2026-04-26 23:07:27 -0600

    Correccion de bugs con el .env

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwind from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+
+const env = loadEnv("", process.cwd(), "");
+
 export default defineConfig({
   plugins: [react(), tailwind()],
   server: {
     allowedHosts: ["remi-unredeemable-amani.ngrok-free.dev"],
     proxy: {
       "/api": {
-        target: import.meta.env.API_URL ?? "http://localhost:5000",
+        target: env.API_URL ?? "http://localhost:5000",
         changeOrigin: true,
       },
     },
