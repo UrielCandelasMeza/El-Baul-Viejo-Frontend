5ffdd7bd26f8949d360a26e543b885b49b97e3db
UrielCandelasMeza <candelas.meza.uriel@gmail.com>
2026-04-26 22:59:52 -0600

    Backend url añadida

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     allowedHosts: ["remi-unredeemable-amani.ngrok-free.dev"],
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: import.meta.env.API_URL ?? "http://localhost:5000",
         changeOrigin: true,
       },
     },
